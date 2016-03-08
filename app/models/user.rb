86429f8b51c9ead0ed98e65eadd81a314c730b1f
Nicolas Mulliez <mulliez.nicolas@gmail.com>
2016-03-08 17:41:48 +0100

    authentificate before action, add divise_views

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,5 +11,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :omniauthable, :timeoutable, :lockable
 end
