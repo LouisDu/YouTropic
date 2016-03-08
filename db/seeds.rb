c3487531a4a6659e7bf8d5c44b38e964dc8cf556
Juliette Picard <picard.juliette@hotmail.fr>
2016-03-08 19:13:02 +0100

    seeds en cours

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
 Category.create(name: "Air")
 Category.create(name: "Terre")
 Category.create(name: "Mer")
+
+Category.create(first_name: "Bob"; last_name: "Dupont"; email: "bob.dupont@gmail.com"; phone_number: "0654433456")
+
