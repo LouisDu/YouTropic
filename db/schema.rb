638d5efe57788b3d47e16dc82512b246a18b057e
Louis Dublancq <louis.dublancq@gmail.com>
2016-03-08 15:06:48 +0100

    Good migrations

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 20160308114502) do
     t.integer  "user_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "photo"
   end
 
   add_index "places", ["category_id"], name: "index_places_on_category_id", using: :btree
