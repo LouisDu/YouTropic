b1a826adc88ff6d614bb4a5ab91af8e98bf9b760
Nicolas Mulliez <mulliez.nicolas@gmail.com>
2016-03-08 16:52:48 +0100

    everything in order (for the moment)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
-  has_many :places, dependent: :destroy
-  has_many :bookings, dependent: :destroy
+   has_many :places, dependent: :destroy
+   has_many :bookings, dependent: :destroy
+   mount_uploader :photo, PhotoUploader
 
-  mount_uploader :photo, PhotoUploader
-
-  validates :frist_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :phone_number, presence: true
-
+   validates :first_name, presence: true
+   validates :last_name, presence: true
+   validates :email, presence: true, uniqueness: true
+   validates :password, presence: true
+   validates :phone_number, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
