3e81e19743daa0fa499fb033d58a0595df97f049
Juliette Picard <picard.juliette@hotmail.fr>
2016-03-09 17:12:10 +0100

    migration de category et index place

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-Category.create(name: "Air")
-Category.create(name: "Terre")
-Category.create(name: "Mer")
+Category.create!(name: "Air")
+Category.create!(name: "Terre")
+Category.create!(name: "Mer")
 
-Category.create(first_name: "Bob"; last_name: "Dupont"; email: "bob.dupont@gmail.com"; phone_number: "0654433456")
+User.create!(first_name: "Bob", last_name: "Dupont", email: "bob.dupont@gmail.com", phone_number: "0654433456", password: "12345678", password_confirmation: "12345678")
 
