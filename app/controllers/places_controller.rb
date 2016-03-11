616d61638895eb71095008bf17bc3a4be04cda82
Augustin Mulliez <mulliez.augustin@gmail.com>
2016-03-11 10:56:53 +0100

    add form for booking

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -8,6 +8,8 @@ class PlacesController < ApplicationController
 
   def show
     @user = current_user
+    @booking = Booking.new
+
     @markers = Gmaps4rails.build_markers(@place) do |place, marker|
       marker.lat place.latitude
       marker.lng place.longitude
